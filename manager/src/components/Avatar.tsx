20cc12e7307b77a17e7d449912551f2fbca6b080
Ivan <cherniv@gmail.com>
2019-10-28 09:52:41 +0200

    Avatar compnent

--- manager/src/components/Avatar.tsx
+++ manager/src/components/Avatar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Auth from '../services/Auth';
 import { Image } from 'react-bootstrap';
 
-const AVATARS_API_PATH = "https://ui-avatars.com/api/?name=";
+const AVATARS_API_PATH = "https://ui-avatars.com/api/?size=20&font-size=0.7&name=";
 
 const Avatar: React.FC = () => {
   var {user} = Auth;
