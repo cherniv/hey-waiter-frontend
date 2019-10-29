4c3eae1dfa1d64526b6421d54d5a9b5cfe8d398a
Ivan <cherniv@gmail.com>
2019-10-29 13:09:53 +0200

    Auth smart logic

--- manager/src/services/Auth.tsx
+++ manager/src/services/Auth.tsx
@@ -1,12 +1,14 @@
 import {observable, computed} from 'mobx';
 import firebase from 'firebase/app';
 import "firebase/auth";
+import "firebase/firestore";
+import User from '../models/User';
 
 class Auth {
 
   @observable authStateLoading:boolean = false;
   @observable user:any; 
-  @observable justSignedUp:boolean = false;
+  @observable justSignedUp:boolean = !true;
   @computed get isLoggedIn () {
     return !!this.user;
   }
@@ -14,10 +16,21 @@ class Auth {
   init () {
     this.authStateLoading = true;
     
-    firebase.auth().onAuthStateChanged(user => {
-      this.user = user;
-      this.authStateLoading = false;
+    firebase.auth().onAuthStateChanged((firebaseUser: any) => {
+      if (firebaseUser) {
+        this.loadUserDataAfterSignin(firebaseUser);
+      } else {
+        // logged out
+        this.user = null;
+        this.authStateLoading = false;
+      }
     });
+  }
+
+  async loadUserDataAfterSignin(firebaseUser: any) {
+    await User.fetchFromRemote(firebaseUser.uid);
+    this.user = User.first;
+    this.authStateLoading = false;
   }
 
   signOut () {
