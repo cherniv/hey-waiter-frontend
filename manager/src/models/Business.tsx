268008cf6b3d4b059be06aaf8449a6019967403c
Ivan <cherniv@gmail.com>
2019-10-31 19:20:25 +0200

    my businesses query

--- manager/src/models/Business.tsx
+++ manager/src/models/Business.tsx
@@ -9,12 +9,38 @@ class Business extends Model {
 
   @observable title:string = '';
   @observable creatorId:any = Auth.user.id;
-
+  @observable users:any = [Auth.user.id];
   @computed get hasTitle() {
     return !!this.title && !!this.title.length;
   }
 
-  
+  static MY_BUSINESSES_QUERY:any = () => ({ 
+    structuredQuery: { 
+      from: [{ collectionId: 'business' }], 
+      where: { 
+        compositeFilter: { 
+          filters: [{ 
+            fieldFilter: { 
+              field: { 
+                fieldPath: 'users' 
+              }, 
+              op: 'array-contains', 
+              value: { 
+                stringValue: Auth.user.id 
+              } 
+            } 
+          }], 
+          op: 'AND' 
+        } 
+      }, 
+      //limit: 4 
+    } 
+  })
+
+  static async getMyBusinesses () {
+    return await this.fetchFromRemote(this.MY_BUSINESSES_QUERY());
+  }
+
 }
 
 export default Business;
