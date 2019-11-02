6e2c2bb482eedf975989425bb8d3261416104294
Ivan <cherniv@gmail.com>
2019-11-02 18:02:48 +0200

    table listen for remote changes

--- manager/src/models/Table.tsx
+++ manager/src/models/Table.tsx
@@ -3,6 +3,9 @@ import Model from 'mobx-active-model';
 import { observable } from 'mobx';
 import Business from './Business';
 
+import firebase from 'firebase/app';
+import "firebase/firestore";
+
 class Table extends Model {
 
   static REMOTE_PATH:string = 'tables/';
@@ -36,6 +39,19 @@ class Table extends Model {
 
   static async fetchTables() {
     this.fetchFromRemote(this.TABLES_QUERY(Business.current.id));
+  }
+
+  constructor (obj:object = null) {
+    super(obj);
+    this.listenForRemoteChanges();
+  }
+
+  listenForRemoteChanges() {
+    const path = Object.getPrototypeOf(this).constructor.REMOTE_PATH + this.id;
+    firebase.firestore().doc(path).onSnapshot((snapshot:any) => {
+      var data = snapshot.data()
+      Object.assign(this, data);
+    });
   }
 }
 
