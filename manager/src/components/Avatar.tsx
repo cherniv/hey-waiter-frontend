4bcc5e9837a8d8cdcff340268c9c27e614e7ab11
Ivan <cherniv@gmail.com>
2020-02-18 11:45:36 +0200

    default avatar for waiters

--- manager/src/components/Avatar.tsx
+++ manager/src/components/Avatar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Auth from '../services/Auth';
 import { Image } from 'react-bootstrap';
+import logo from '../images/sidebar-logo.png';
 
 type Props = {
     size?: number,
@@ -11,7 +12,11 @@ const Avatar: React.FC<Props> = (props) => {
   var {firebaseUser} = Auth;
   var size = props.size || 18;
   const AVATARS_API_PATH = `https://ui-avatars.com/api/?size=${size}&font-size=0.7&name=`;
-  var path = (firebaseUser && firebaseUser.photoURL) || (AVATARS_API_PATH + firebaseUser.displayName);
+  const {
+		photoURL,
+		displayName,
+  } = firebaseUser;
+  var path = (photoURL) || (displayName && (AVATARS_API_PATH + displayName)) || logo;
   return (
     <Image src={path} width={size} height={size} roundedCircle /> 
   )
