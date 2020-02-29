d907ab70185b575d7dc6d2a58c1380d72bc0e3c7
Shalmu <shalmoo@gmail.com>
2020-02-29 19:52:36 +0300

    Proofread

--- manager/src/components/Waiters.tsx
+++ manager/src/components/Waiters.tsx
@@ -126,7 +126,7 @@ class WaiterComponent extends React.Component<tableProps> {
   @observable tempValue:string = this.props.waiter.customName;
 
   removeWaiter(waiter:Waiter) {
-    if(!window.confirm(`Do you really want to remove this waiter?\nThis act is irreversible!\nInstead, you can simply regenerate the code.\n\nAre you sure?`))return;
+    if(!window.confirm(`This act is irreversible!\nInstead, you can simply regenerate the code.\n\nDo you really want to remove this waiter?`))return;
     Business.current.removeWaiter(waiter);
     trek({as:`mng`, mng:Business.current.id, act:`-waiter`, wtr:waiter.id});
   }
