01263f461b50ff6bc74387d197127034b8725ea5
Ivan <cherniv@gmail.com>
2020-03-02 12:58:45 +0200

    web manager favicon

--- manager/public/custom-sw.js
+++ manager/public/custom-sw.js
@@ -2,8 +2,8 @@ self.addEventListener('push', event => {
   const data = event.data.json()
   const options = {
     body: data.body,
-    icon: '/favicon.ico',
-    image: '/favicon.ico'
+    icon: 'favicon.ico',
+    image: 'favicon.ico'
   }
   event.waitUntil(
     self.registration.showNotification(data.title, options)
