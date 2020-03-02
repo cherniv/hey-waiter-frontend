94d4673d153ff45092f86501fd29b031f8aaafd9
Ivan <cherniv@gmail.com>
2020-03-02 13:57:19 +0200

    web notification on click should open waiter window

--- manager/public/custom-sw.js
+++ manager/public/custom-sw.js
@@ -1,11 +1,75 @@
 self.addEventListener('push', event => {
-  const data = event.data.json()
+  const {title, tableId} = event.data.json();
   const options = {
-    body: data.body,
+    data: {tableId},
     icon: 'favicon.ico',
-    image: 'favicon.ico'
+    image: 'favicon.ico',
+    //requireInteraction: true,
+    // actions: [
+    //   {
+    //     action: 'reset-table-action',
+    //     title: 'Reset table',
+    //     //icon: '/images/demos/action-1-128x128.png'
+    //   },
+    // ]
   }
   event.waitUntil(
-    self.registration.showNotification(data.title, options)
+    self.registration.showNotification(title, options)
   );
 })
+
+self.addEventListener('notificationclick', function(event) {  
+  const notificationData = event.notification.data;
+  if (!event.action) {
+    // Was a normal notification click
+    const clickedNotification = event.notification;
+    clickedNotification.close();
+    gotoWindow(event);
+    return;
+  }
+
+  
+  switch (event.action) {
+    case 'reset-table-action':
+      console.log('User ❤️️\'s coffees.');
+      break;
+    case 'doughnut-action':
+      console.log('User ❤️️\'s doughnuts.');
+      break;
+    case 'gramophone-action':
+      console.log('User ❤️️\'s music.');
+      break;
+    case 'atom-action':
+      console.log('User ❤️️\'s science.');
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
+});
+
+function gotoWindow (event) {
+  const urlToOpen = new URL("/manager/", self.location.origin).href;
+  const promiseChain = clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+  }).then((windowClients) => {
+    let matchingClient = null;
+
+    for (let i = 0; i < windowClients.length; i++) {
+      const windowClient = windowClients[i];
+      if (windowClient.url === urlToOpen) {
+        matchingClient = windowClient;
+        break;
+      }
+    }
+
+    if (matchingClient) {
+      return matchingClient.focus();
+    } else {
+      return clients.openWindow(urlToOpen);
+    }
+  });
+
+  event.waitUntil(promiseChain)
+}
